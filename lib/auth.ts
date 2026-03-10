6813eb8d2b927589b95a8e76782243720a32e654
AldinHyuseinov <Aldin6687@gmail.com>
2026-03-10 23:40:02 +0200

    add Prisma setup adapter and user authentication models
    - Updated package.json to include Prisma, dotenv, and better-auth dependencies.
    - Created prisma.config.ts for Prisma configuration with environment variable support.
    - Added schema.prisma defining User, Session, Account, and Verification models for user authentication.

---
+++ lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import { username } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "mysql",
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    facebook: {
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [username()],
+});
